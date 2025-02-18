ec7a89ab6b109e0078340a2cf54d77ae4bcd5822
DSM4 <fatec-dsm4@academico.intra>
2025-02-17 22:31:42 -0300

    Aula 01.3 style e forms

--- aula-01.1/controllers/routes.py
+++ aula-01.1/controllers/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 # Criando a primeira rota da aplicação #
 players = []
+gameList = [{"title": 'CS-GO', "year": 2012, "category": 'FPS'}]
 
 
 def init_app(app):
@@ -12,8 +13,16 @@ def init_app(app):
 
     @app.route('/games', methods=['GET', 'POST'])
     def games():
-        game = {"title": 'CS-GO', "year": 2012, "category": 'FPS'}
         if request.method == 'POST':
             if request.form.get('player'):
                 players.append(request.form.get('player'))
-        return render_template('games.html',  game=game, players=players)
+                return redirect(url_for('games'))
+        return render_template('games.html',  gameList=gameList, players=players)
+
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            form_data = request.form.to_dict()
+            gameList.append(form_data)
+            return redirect(url_for('cadgames'))
+        return render_template('cadgames.html', gameList=gameList)
