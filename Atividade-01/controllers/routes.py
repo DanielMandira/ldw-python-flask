51b5b6a76fb6e9e08cb3f8fea3104c8e4d941b94
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2025-02-27 13:04:56 -0300

    Tela MovieInfo

--- Atividade-01/controllers/routes.py
+++ Atividade-01/controllers/routes.py
@@ -8,10 +8,29 @@ favoritos = [{"title": 'Castle in the Sky',
                "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
               }]
 
+def fetch_movies():
+    url = 'https://ghibliapi.vercel.app/films'
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    return json.loads(data)
+
+def fetch_movie(movie_id):
+    url = f'https://ghibliapi.vercel.app/films/{movie_id}'
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        return json.loads(data)
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            return None
+        raise
+
+
 def init_app(app):
     @app.route('/')
     def home():
-        return render_template('index.html')
+        moviesjson = fetch_movies()
+        return render_template('index.html', moviesjson=moviesjson)
     
     @app.route('/favoritos', methods=['GET', 'POST'])
     def favoritos():
@@ -30,21 +49,14 @@ def init_app(app):
         return render_template('cadfavoritos.html', favoritos=favoritos)
     
     @app.route('/movies', methods=['GET', 'POST'])
-    @app.route('/movie/<int:id>', methods=['GET', 'POST'])
-    def movies(id=None):
-        url = 'https://ghibliapi.vercel.app/films'
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        moviesjson = json.loads(data)
-        print(moviesjson)
-        if id:
-            movieinfo = []
-            for c in moviesjson:
-                if c['id'] == id:
-                    movieinfo=c
-                    break
-            if movieinfo:
-                return render_template('movieinfo.html', movieinfo=movieinfo)
-            else:
-                return 'movie not found'
+    def movies():
+        moviesjson = fetch_movies()
         return render_template('movies.html', moviesjson=moviesjson)
+    @app.route('/movie/<id>', methods=['GET', 'POST'])
+    def movie(id):
+        movieinfo = fetch_movie(id)
+        if movieinfo:
+            return render_template('movieinfo.html', movieinfo=movieinfo)
+        else:
+            return 'Filme não encontrado', 404
+        
