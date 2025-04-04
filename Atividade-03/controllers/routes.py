d7d20dea0eebd95f593b911b5dac0e64191d258d
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2025-04-04 12:16:03 -0300

    Atividade 03

--- Atividade-03/controllers/routes.py
+++ Atividade-03/controllers/routes.py
@@ -15,8 +15,74 @@ def init_app(app):
 
     @app.route('/')
     def galeria():
-        cars = Car.query.all()
+        query = Car.query
+        
+        # Filtros
+        categoria = request.args.get('categoria')
+        marca = request.args.get('marca')
+        ano = request.args.get('ano')
+        
+        if categoria:
+            query = query.filter(Car.categoria == categoria)
+        if marca:
+            query = query.filter(Car.marca.ilike(f'%{marca}%'))
+        if ano:
+            query = query.filter(Car.ano == ano)
+        
+        cars = query.all()
         return render_template('index.html', cars=cars)
+
+    @app.route('/carro/<int:id>')
+    def carro(id):
+        car = Car.query.get_or_404(id)
+        return render_template('carro.html', car=car)
+
+    @app.route('/carro/<int:id>/editar', methods=['GET', 'POST'])
+    def editar_carro(id):
+        car = Car.query.get_or_404(id)
+        
+        if request.method == 'POST':
+            car.marca = request.form.get('marca')
+            car.modelo = request.form.get('modelo')
+            car.ano = request.form.get('ano')
+            car.descricao = request.form.get('descricao')
+            car.potencia = request.form.get('potencia')
+            car.categoria = request.form.get('categoria')
+            
+            file = request.files.get('foto')
+            if file and allowed_file(file.filename):
+                # Remove a foto antiga
+                if car.foto:
+                    old_file = os.path.join(app.config['UPLOAD_FOLDER'], car.foto)
+                    if os.path.exists(old_file):
+                        os.remove(old_file)
+                
+                # Salva a nova foto
+                filename = secure_filename(file.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+                car.foto = filename
+            
+            db.session.commit()
+            flash("Carro atualizado com sucesso!", "success")
+            return redirect(url_for('carro', id=car.id))
+        
+        return render_template('editar_carro.html', car=car)
+
+    @app.route('/carro/<int:id>/excluir', methods=['POST'])
+    def excluir_carro(id):
+        car = Car.query.get_or_404(id)
+        
+        # Remove a foto do carro
+        if car.foto:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], car.foto)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        
+        db.session.delete(car)
+        db.session.commit()
+        flash("Carro excluído com sucesso!", "success")
+        return redirect(url_for('galeria'))
 
     @app.route('/cadastro', methods=['GET', 'POST'])
     def cadastro():
